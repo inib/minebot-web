b16d9bbf5f067ddeb30d7535c3a787ecdbf13bce
inib <alpinehd@gmail.com>
2016-10-14 19:56:59 +0200

    command api and frontend

--- public/javascripts/commands.js
+++ public/javascripts/commands.js
@@ -16,56 +16,21 @@ $(document).ready(function() {
 
 // functions 
 
-function displayUser(user) {
-    var userContent = '';    
-
-    $.getJSON('/userstats/' + user, function (data) {
-        if (data.userdetails.length > 0) {            
-            userContent += '<li>Name: ' + data.userdetails[0].user.name + '</li>';
-            userContent += '<li>Usergroup: ' + data.userdetails[0].user.group + '</li>';
-            userContent += '<li>Subscriber: ' + data.userdetails[0].user.subscribed + '</li>';
-            userContent += '<li>Schokiplatzierung: ' + data.userdetails[0].user.rank + '</li>';
-            userContent += '<li>Benutzerrrang: ' + data.userdetails[0].user.customrank + '</li>';
-            userContent += '<li>Brawlwins: ' + data.userdetails[0].user.brawlwins + '</li>';
-            userContent += '<li>zuletzt online: ' + data.userdetails[0].user.lastseen + '</li>';
-            userContent += '<li>Zeit im Chat: ' + data.userdetails[0].user.time + '</li>';
-            $('#userstats ul').html(userContent);
-        }
-        else {
-            userContent = "<li>Name nicht gefunden :(</li>";
-            $('#userstats ul').html(userContent);
-        }
-    });
-}
-
-function getIcon(name, group, sub) {
-
+function getIcon(group) {
     var icoHTML = '';
-
-    if (name == 'm1nebot') {
-        return '<span class="ico bot"></span>';
+    switch (group) {
+        case '3':
+            icoHTML += '<span class="ico sub"></span>Subscriber';
+            break;
+        case '2':
+            icoHTML += '<span class="ico mod"></span>Moderator';
+            break;
+        case ('1' || '0'):
+            icoHTML += '<span class="ico broad"></span><span class="ico staff"></span>Broadcaster/Admin';
+            break;
+        default: icoHTML = 'Viewer';
     }
-    else {
-        switch (group) {
-            case '2':
-                icoHTML += '<span class="ico mod"></span>';
-                break;
-            case '1':
-                icoHTML += '<span class="ico staff"></span>';
-                break;
-            case '0':
-                icoHTML += '<span class="ico broad"></span><span class="ico sub"></span>';
-                break;
-            default: icoHTML = '';
-        }
-
-        if (sub) {
-            icoHTML += '<span class="ico sub"></span>';
-        }
-
-        return icoHTML;
-    }
-
+    return icoHTML;
 }
 
 function populateTable() {
@@ -73,25 +38,16 @@ function populateTable() {
     var tableContent = '';
 
     // jQuery AJAX call for JSON
-    $.getJSON( '/users/0/30', function( data ) {        
-        $.each(data.rankedpointslist, function(){
+    $.getJSON( '/commands/0/30', function( data ) {        
+        $.each(data.commandlist, function(){
             tableContent += '<tr>';
-            tableContent += '<td>' + this.user.rank + '</td>';
-            tableContent += '<td>' + getIcon(this.user.name, this.user.group, this.user.subscribed) + '<a href="#" id="linkshowuser" rel="' + this.user.name + '">' + this.user.name + '</a></td>';
-            tableContent += '<td>' + this.user.points + '</td>';
+            tableContent += '<td>' + data.prefix + this.command.name + '</td>';
+            tableContent += '<td>' + getIcon(this.command.permission) + '</td>';
+            tableContent += '<td>' + this.command.cooldown + '</td>';
+            tableContent += '<td>' + this.command.price + '</td>';
             tableContent += '</tr>';            
         });
 
-        $('#userList table tbody').html(tableContent);
-
-        $("a#linkshowuser").click(function () {
-            var user = $(this).attr('rel');
-            if (!user) {
-                //error
-            }
-            else {
-                displayUser(user);
-            }
-        });
+        $('#commandList table tbody').html(tableContent);
     });
 };
--- routes/api_commands.js
+++ routes/api_commands.js
@@ -2,10 +2,55 @@ var express = require('express');
 var router = express.Router();
 var db = require('../modules/db');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    userName = req.params.userName;
-    res.render('commands', { title: 'Befehle' });
+/* GET Data. */
+router.get('/:permission?', function(req, res, next) {
+
+  var perm = parseInt(!isNaN(req.params.permission) ? req.params.permission : -1);
+  var query =   'SELECT 		t1.variable AS \'command\','+
+		        't1.value AS \'response\','+
+			    't2.value AS \'cooldown\','+
+                't3.value AS \'permission\','+
+                't4.value AS \'price\' '+
+                'FROM 		phantombot_command 	t1 '+
+                'LEFT JOIN	phantombot_cooldown	t2 '+
+                'ON			t1.variable = t2.variable '+
+                'LEFT JOIN  	phantombot_permcom    t3 '+
+                'ON			t1.variable = t3.variable '+
+                'LEFT JOIN  	phantombot_pricecom    t4 '+
+                'ON			t1.variable = t4.variable ';
+
+  if (perm >= 0) {
+      query +=  'WHERE t3.value = \'' + perm + '\'';
+  } else {
+      query += ';';
+  }
+  
+  db.queryCache(query, function (err, rows, fields) {
+    if (err) {
+        res.status(500).json({ error: 'server error', response: err });
+    }
+    else {
+      var dbJSONObj = [];
+
+      for (var index = 0; index < rows[1].length; index++) {
+        var element = rows[1][index];
+        var jsonEl = {
+          command: {
+            name: element.command,
+            permission: element.permission,
+            cooldown: (element.cooldown === null) ? '0' : element.cooldown,
+            price: (element.price === null) ? '0' : element.price
+          }
+        };
+        dbJSONObj.push(jsonEl);
+      }
+      res.json({
+          description: 'list of custom commands filtered by given permission level',
+          filter: perm,
+          commandprefix: '!',
+          commandlist: dbJSONObj});
+    }
+  });
 });
 
 module.exports = router;
