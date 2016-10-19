7a3fb323757159096685e0c5df61809bc23bdfb2
inib <alpinehd@gmail.com>
2016-10-19 21:56:11 +0200

    table toplist

--- public/javascripts/toplist.js
+++ public/javascripts/toplist.js
@@ -16,32 +16,8 @@ $(document).ready(function() {
 
 // functions 
 
-function displayUser(user) {
-    var userContent = '';    
-
-    $.getJSON('/api/v1/userstats/' + user, function (data) {
-        if (data.userdetails.length > 0) {            
-            userContent += '<li>Name: ' + data.userdetails[0].user.name + '</li>';
-            userContent += '<li>Usergroup: ' + data.userdetails[0].user.group + '</li>';
-            userContent += '<li>Subscriber: ' + data.userdetails[0].user.subscribed + '</li>';
-            userContent += '<li>Schokiplatzierung: ' + data.userdetails[0].user.rank + '</li>';
-            userContent += '<li>Benutzerrrang: ' + data.userdetails[0].user.customrank + '</li>';
-            userContent += '<li>Brawlwins: ' + data.userdetails[0].user.brawlwins + '</li>';
-            userContent += '<li>zuletzt online: ' + data.userdetails[0].user.lastseen + '</li>';
-            userContent += '<li>Zeit im Chat: ' + data.userdetails[0].user.time + '</li>';
-            $('#userstats ul').html(userContent);
-        }
-        else {
-            userContent = "<li>Name nicht gefunden :(</li>";
-            $('#userstats ul').html(userContent);
-        }
-    });
-}
-
 function getIcon(name, group, sub) {
-
     var icoHTML = '';
-
     if (name == 'm1nebot') {
         return '<span class="ico bot"></span>';
     }
@@ -58,14 +34,33 @@ function getIcon(name, group, sub) {
                 break;
             default: icoHTML = '';
         }
-
         if (sub) {
             icoHTML += '<span class="ico sub"></span>';
         }
-
         return icoHTML;
     }
+}
 
+function displayUser(user) {
+    var userContent = '';    
+
+    $.getJSON('/api/v1/userstats/' + user, function (data) {
+        if (data.userdetails.length > 0) {            
+            userContent += '<tr><td>Name</td><td>' + data.userdetails[0].user.name + '</td></tr>';
+            userContent += '<tr><td>Usergroup</td><td>' + data.userdetails[0].user.group + '</td></tr>';
+            userContent += '<tr><td>Subscriber</td><td>' + data.userdetails[0].user.subscribed + '</td></tr>';
+            userContent += '<tr><td>Schokiplatzierung</td><td>' + data.userdetails[0].user.rank + '</td></tr>';
+            userContent += '<tr><td>Benutzerrrang</td><td>' + data.userdetails[0].user.customrank + '</td></tr>';
+            userContent += '<tr><td>Brawlwins</td><td>' + data.userdetails[0].user.brawlwins + '</td></tr>';
+            userContent += '<tr><td>zuletzt online</td><td>' + data.userdetails[0].user.lastseen + '</td></tr>';
+            userContent += '<tr><td>Zeit im Chat</td><td>' + data.userdetails[0].user.time + '</td></tr>';
+            $('#userStats table tbody').html(userContent);
+        }
+        else {
+            userContent = "<tr><td>Name nicht gefunden :(</td></tr>";
+            $('#userStats table tbody').html(userContent);
+        }
+    });
 }
 
 function populateTable() {
